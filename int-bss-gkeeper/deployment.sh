c657368df7b0a94995844a3bf30d94896175a6a0
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-26 12:39:48 +0200

    tests

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -13,6 +13,6 @@ int-bss-gkeeper/scripts/postCatalogueSampleDescriptors.sh
 int-bss-gkeeper/scripts/postGatekeeperSampleRequest.sh
 
 # -- BSS
-if ! [[ "$(docker inspect -f {{.State.Running}} intbssgkeeper_son-bss 2> /dev/null)" == "" ]]; then docker rm -fv intbssgkeeper_son-bss ; fi
-docker run -d --name intbssgkeeper_son-bss -p 25001:1337 -p 25002:1338 -v "$(pwd)/reports:/usr/local/yeoman/SonataBSS/E2E_tests/reports" -it son-yo-gen-bss
-docker exec -t intbssgkeeper_son-bss sudo chown yeoman:yeoman /usr/local/yeoman/SonataBSS/E2E_tests/reports
+if ! [[ "$(docker inspect -f {{.State.Running}} son-bss 2> /dev/null)" == "" ]]; then docker rm -fv son-bss ; fi
+docker run -d --name son-bss -p 25001:1337 -p 25002:1338 -v "$(pwd)/reports:/usr/local/yeoman/SonataBSS/E2E_tests/reports" -it son-yo-gen-bss
+docker exec -t son-bss sudo chown yeoman:yeoman /usr/local/yeoman/SonataBSS/E2E_tests/reports
