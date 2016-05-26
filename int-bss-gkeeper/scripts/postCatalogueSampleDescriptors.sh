5a9c12192c4fe693a69d27b12535ec4d80e66c04
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-26 13:00:24 +0200

    tests

--- int-bss-gkeeper/scripts/postCatalogueSampleDescriptors.sh
+++ int-bss-gkeeper/scripts/postCatalogueSampleDescriptors.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # -- insert NSD/VNFD
-curl -H "Content-Type: application/json" -X POST --data-binary @resources/NSD.json http://sp.int2.sonata-nfv.eu:4002/catalogues/network-services
-curl -H "Content-Type: application/json" -X POST --data-binary @resources/VNFD.json http://sp.int2.sonata-nfv.eu:4002/catalogues/vnfs
+curl -H "Content-Type: application/json" -X POST --data-binary @int-bss-gkeeper/resources/NSD.json http://sp.int2.sonata-nfv.eu:4002/catalogues/network-services
+curl -H "Content-Type: application/json" -X POST --data-binary @int-bss-gkeeperresources/VNFD.json http://sp.int2.sonata-nfv.eu:4002/catalogues/vnfs
