9e09d6052cad7018e7339711fbe18be12eb23f96
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-26 13:32:02 +0200

    tests

--- int-bss-gkeeper/tests.sh
+++ int-bss-gkeeper/tests.sh
@@ -3,7 +3,7 @@ docker exec -t -d son-bss grunt serve:integration_tests --suite=intBSS_GK
 
 # -- get the remote reports
 x=0
-while [ "$x" -lt 100 -a ! -e int-bss-gkeeper/reports/htmlReport.html ]; do
+while [ "$x" -lt 100 -a ! -e $(pwd)/reports/htmlReport.html ]; do
         x=$((x+1))		
         sleep 1
 done
