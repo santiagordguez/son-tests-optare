90fcfc02777add2d8471cc462d4588087abf061f
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-26 10:40:13 +0200

    tests

--- int-bss-gkeeper/scripts/tests.sh
+++ int-bss-gkeeper/scripts/tests.sh
@@ -2,7 +2,7 @@
 export DOCKER_HOST="tcp://sp.int2.sonata-nfv.eu:2375"
 docker exec -t -d intbssgkeeper_son-bss grunt serve:integration_tests --suite=intBSS_GK
 
-# -- get remote reports
+# -- get the remote reports
 cd /var/lib/jenkins/jobs/int-bss-gkeeper/workspace/int-bss-gkeeper/reports/
 x=0
 while [ "$x" -lt 100 -a ! -e .htmlReport.html ]; do
