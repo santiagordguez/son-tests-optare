e68c59bbfd16d89581d50cd9f0bdcb44046924b2
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-05-30 08:18:46 +0200

    tests

--- int-bss-gkeeper/tests.sh
+++ int-bss-gkeeper/tests.sh
@@ -5,5 +5,7 @@ docker exec -t -d son-bss grunt serve:integration_tests --suite=intBSS_GK
 x=0
 while [ "$x" -lt 100 -a ! -e $(pwd)/reports/htmlReport.html ]; do
         x=$((x+1))		
-        sleep 1
+		docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports/htmlReport.html .
+        sleep 1		
 done
+docker cp son-bss:/usr/local/yeoman/SonataBSS/E2E_tests/reports .
