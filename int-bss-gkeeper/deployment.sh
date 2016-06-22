1c9e5db645fec4a777272ed9c2dd1ef9a57c8b1b
Santiago Rodríguez <srodriguez@optaresolutions.com>
2016-06-22 20:17:58 +0200

    E2E tests

--- int-bss-gkeeper/deployment.sh
+++ int-bss-gkeeper/deployment.sh
@@ -13,7 +13,7 @@ int-bss-gkeeper/scripts/postCatalogueSampleDescriptors.sh
 int-bss-gkeeper/scripts/postGatekeeperSampleRequest.sh
 sleep 5
 # -- BSS
-if ! [[ "$(docker inspect -f {{.State.Running}} son-bss 2> /dev/null)" == "" ]]; then docker rm -fv son-bss ; fi
+#if ! [[ "$(docker inspect -f {{.State.Running}} son-bss 2> /dev/null)" == "" ]]; then docker rm -fv son-bss ; fi
 #docker run -d --name son-bss -p 25001:1337 -p 25002:1338 -v "$(pwd)/reports:/usr/local/yeoman/SonataBSS/E2E_tests/reports" -it son-yo-gen-bss
-docker run -d --name son-bss -p 25001:1337 -p 25002:1338 son-yo-gen-bss grunt serve:development --gkApiUrl=http://chdocker.optare.loc:25002/mock
+#docker run -d --name son-bss -p 25001:1337 -p 25002:1338 son-yo-gen-bss grunt serve:development --gkApiUrl=http://chdocker.optare.loc:25002/mock
 #docker exec -t son-bss sudo chown yeoman:yeoman /usr/local/yeoman/SonataBSS/E2E_tests/reports
--- int-bss-gkeeper/tests.sh
+++ int-bss-gkeeper/tests.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #docker exec -t -d son-bss grunt serve:integration_tests --suite=intBSS_GK
-docker exec -t -d son-bss grunt protractor_webdriver protractor:run --suite=intBSS_GK
-
+docker run -d --name son-bss -p 25001:1337 -p 25002:1338 --log-driver=gelf --log-opt gelf-address=udp://172.20.49.181:12900 son-yo-gen-bss grunt serve:integration_tests --gkApiUrl=http://172.20.49.181:25002/mock --suite=unitTests --debug
 # -- get the remote reports
 x=0
 while [ "$x" -lt 100 -a ! -e $(pwd)/reports/htmlReport.html ]; do
